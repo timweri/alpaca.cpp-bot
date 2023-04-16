fd501a852a8250ed9f621432770a51a9abbdb710
Duc Thanh Nguyen <ng.duc.tahn@gmail.com>
2023-04-16 01:04:25 -0400

    Make async

--- alpaca_cpp_interface/interface.py
+++ alpaca_cpp_interface/interface.py
@@ -1,6 +1,6 @@
-import subprocess
 import sys
 from enum import Enum
+import asyncio
 
 class AlpacaCppInterface:
     class State(Enum):
@@ -10,18 +10,17 @@ class AlpacaCppInterface:
     def __init__(self, alpaca_exec_path, model_path):
         self.alpaca_exec_path = alpaca_exec_path
         self.model_path = model_path
+        self.state = AlpacaCppInterface.State.TERMINATED
 
-        self.start()
+    async def restart(self):
+        await self.terminate()
+        await self.start()
 
-    def restart(self):
-        self.terminate()
-        self.start()
-
-    def start(self):
-        self.cli_process = subprocess.Popen(
-                [self.alpaca_exec_path, '-m', self.model_path],
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
+    async def start(self):
+        self.cli_process = await asyncio.create_subprocess_shell(
+                ' '.join([self.alpaca_exec_path, '-m', self.model_path]),
+                stdin=asyncio.subprocess.PIPE,
+                stdout=asyncio.subprocess.PIPE,
                 stderr=sys.stderr
             )
         # Signal whether the alpaca.cpp process is active
@@ -29,24 +28,24 @@ class AlpacaCppInterface:
 
         # Signal whether alpaca.cpp is ready for another prompt
         self.ready_for_prompt = True
-        self._initial_flush_readline()
+        await self._initial_flush_readline()
     
     # Flush the initial prints before first user prompt
-    def _initial_flush_readline(self):
+    async def _initial_flush_readline(self):
         # Shouldn't be reading if process is killed
         if self.state != AlpacaCppInterface.State.ACTIVE:
             return
 
         # Flush 1 empty line
-        self.cli_process.stdout.readline()
+        await self.cli_process.stdout.readline()
 
         # Flush "> "
-        self.cli_process.stdout.read(2)
+        await self.cli_process.stdout.read(2)
 
 
     # Read the alpaca.cpp generated text
     # Blocks until alpaca.cpp finishes
-    def read(self):
+    async def read(self):
         # Shouldn't be reading if process is killed or alpaca.cpp is waiting for
         # user input
         if self.state != AlpacaCppInterface.State.ACTIVE or self.ready_for_prompt:
@@ -62,7 +61,7 @@ class AlpacaCppInterface:
             byte_array = bytearray()
             while True:
                 try:
-                    byte_array += self.cli_process.stdout.read(1)
+                    byte_array += await self.cli_process.stdout.read(1)
                     new_char = byte_array.decode('utf-8')
                     break
                 except UnicodeDecodeError:
@@ -71,7 +70,8 @@ class AlpacaCppInterface:
             # User input prompt detection
             if prev_new_line and new_char == ">":
                 # Check if the next char is " "
-                next_char = self.cli_process.stdout.read(1).decode('utf-8')
+                next_char = await self.cli_process.stdout.read(1)
+                next_char = next_char.decode('utf-8')
                 if next_char == " ":
                     break
 
@@ -90,7 +90,7 @@ class AlpacaCppInterface:
         return output
 
     # Enters thhe next
-    def write(self, prompt):
+    async def write(self, prompt):
         # Shouldn't be writing if process is killed or alpaca.cpp is not
         # ready for user input
         if self.state != AlpacaCppInterface.State.ACTIVE or not self.ready_for_prompt:
@@ -99,22 +99,22 @@ class AlpacaCppInterface:
         prompt = prompt.strip()
         # print(f"Wrote \"{prompt}\" to chat")
         self.cli_process.stdin.write((prompt+"\n").encode('utf-8'))
-        self.cli_process.stdin.flush()
+        await self.cli_process.stdin.drain()
 
         # Now we wait for the model to generate text
         self.ready_for_prompt = False
 
         return True
 
-    def terminate(self):
+    async def terminate(self):
         if self.state != AlpacaCppInterface.State.ACTIVE:
             return
 
         self.state = AlpacaCppInterface.State.TERMINATED
 
-        self.cli_process.stdin.close()
-        self.cli_process.terminate()
-        self.cli_process.wait(timeout=0.2)
+        await self.cli_process.stdin.close()
+        await self.cli_process.terminate()
+        await self.cli_process.wait(timeout=0.2)
 
     def __del__(self):
-        self.terminate()
+        asyncio.run(self.terminate())
--- alpaca_cpp_interface/telegram_bot.py
+++ alpaca_cpp_interface/telegram_bot.py
@@ -1,63 +1,78 @@
+import asyncio
 from dotenv import load_dotenv
-import telebot
+from telebot.async_telebot import AsyncTeleBot
 import os
+import sys
 from interface import AlpacaCppInterface
 import time
+import traceback
 load_dotenv()
 
 TELEGRAM_BOT_API_TOKEN = os.getenv('TELEGRAM_BOT_API_TOKEN')
 ALPACA_CPP_EXEC_PATH = os.getenv('ALPACA_CPP_EXEC_PATH')
 MODEL_PATH = os.getenv('MODEL_PATH')
 
-if __name__ == "__main__":
-    bot = telebot.TeleBot(TELEGRAM_BOT_API_TOKEN, parse_mode=None) 
+async def main():
+    bot = AsyncTeleBot(TELEGRAM_BOT_API_TOKEN, parse_mode=None) 
 
     alpaca_cpp_interface = AlpacaCppInterface(ALPACA_CPP_EXEC_PATH, MODEL_PATH)
+    await alpaca_cpp_interface.start()
 
     @bot.message_handler(commands=['restart'])
-    def send_restart(message):
-        alpaca_cpp_interface.restart()
-        bot.reply_to(message, "I just restarted the alpaca.cpp instance.")
+    async def send_restart(message):
+        await alpaca_cpp_interface.restart()
+        await bot.reply_to(message, "I just restarted the alpaca.cpp instance.")
 
     @bot.message_handler(commands=['kill'])
-    def send_restart(message):
+    async def send_restart(message):
         if alpaca_cpp_interface.state == AlpacaCppInterface.State.ACTIVE:
-            alpaca_cpp_interface.terminate()
-            bot.reply_to(message, "I just killed the alpaca.cpp instance.")
+            await alpaca_cpp_interface.terminate()
+            await bot.reply_to(message, "I just killed the alpaca.cpp instance.")
         else:
-            bot.reply_to(message, "The alpaca.cpp instance is already inactive.")
+            await bot.reply_to(message, "The alpaca.cpp instance is already inactive.")
 
     @bot.message_handler(commands=['state'])
-    def send_state(message):
+    async def send_state(message):
         if alpaca_cpp_interface.state == AlpacaCppInterface.State.ACTIVE:
-            bot.reply_to(message, "The alpaca.cpp instance is active.")
+            await bot.reply_to(message, "The alpaca.cpp instance is active.")
         else:
-            bot.reply_to(message, "The alpaca.cpp instance is inactive.")
+            await bot.reply_to(message, "The alpaca.cpp instance is inactive.")
 
     @bot.message_handler(commands=['start'])
-    def send_welcome(message):
-        bot.reply_to(message, "Howdy, how are you doing? I am a bootleg alpaca.cpp bot. Please enter your prompt and don't expect much.")
+    async def send_welcome(message):
+        await bot.reply_to(message, "Howdy, how are you doing? I am a bootleg alpaca.cpp bot. Please enter your prompt and don't expect much.")
 
     @bot.message_handler(commands=['help'])
-    def send_help(message):
-        bot.reply_to(message, "I haven't got time to fill this up yet.")
+    async def send_help(message):
+        await bot.reply_to(message, '''Here are the available commands:
+/start start the AlpacaCpp instance if inactive
+/restart restart the AlpacaCpp instance
+/state check if the AlpacaCpp instance is alive
+/kill kill the AlpacaCpp instance
+
+Chat normally to talk to the AlpacaCpp instance if active.''')
 
     @bot.message_handler(func=lambda m: True)
-    def echo_all(message):
+    async def echo_all(message):
         if alpaca_cpp_interface.state != AlpacaCppInterface.State.ACTIVE:
-            bot.reply_to(message, "The alpaca.cpp instance is currently inactive.")
+            await bot.reply_to(message, "The alpaca.cpp instance is currently inactive.")
             return
 
         if alpaca_cpp_interface.ready_for_prompt:
             try:
-                if not alpaca_cpp_interface.write(message.text):
-                    bot.reply_to(message, "Failed to process prompt.")
+                if not await alpaca_cpp_interface.write(message.text):
+                    await bot.reply_to(message, "Failed to process prompt.")
                     return
 
-                bot.reply_to(message, alpaca_cpp_interface.read())
+                await bot.reply_to(message, await alpaca_cpp_interface.read())
             except Exception as e:
-                bot.reply_to(message, "There's an unexpected error: " + str(e))
+                traceback.print_exc()
+                await bot.reply_to(message, "There's an unexpected error: " + str(e))
         else:
-            bot.reply_to(message, "I'm still generating answer for the previous prompt. I'll ignore this prompt.")
+            await bot.reply_to(message, "I'm still generating answer for the previous prompt. I'll ignore this prompt.")
 
-    bot.infinity_polling()
+    await bot.polling()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
